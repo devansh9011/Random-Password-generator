04ec5666bb182263e63329634383004b3bccc5ed
Devansh Sharma <42490656+devansh9011@users.noreply.github.com>
2020-12-22 23:22:16 +0530

    Added more options to select from

--- pass.cpp
+++ pass.cpp
@@ -7,43 +7,43 @@ using namespace std;
 
 int main()
 {
-	vector<char> number={'0','1', '2', '3', '4', '5', '6', '7',
-    				  	'8','9'};
-	vector<char> lower={'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',  
-						'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 
-						'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 
-						'z'};
+    vector<char> number={'0','1', '2', '3', '4', '5', '6', '7',
+    			'8','9'};
+    vector<char> lower={'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',  
+			'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 
+			'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 
+			'z'};
     vector<char> upper={'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',  
-						'I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q', 
-						'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 
-						'Z'};
+			'I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q', 
+			'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 
+			'Z'};
     vector<char> symbol={'!','@', '#', '$', '%', '^', '&', '*'};
 
-	char choice;
-	vector<vector<char>> s;
+    char choice;
+    vector<vector<char>> s;
 
-	cout<<"\nDo you want to include numbers(y/n) : ";
-	cin>>choice;
-	 if(choice=='Y'||choice=='y')
+    cout<<"\nDo you want to include numbers(y/n) : ";
+    cin>>choice;
+    if(choice=='Y'||choice=='y')
     	s.push_back(number);
 
     cout<<"\nDo you want to include lower case alphabets(y/n) : ";
-	cin>>choice;
+    cin>>choice;
     if(choice=='Y'||choice=='y')
     	s.push_back(lower);
 
-	cout<<"\nDo you want to include upper case alphabets(y/n) : ";
-	cin>>choice;
+    cout<<"\nDo you want to include upper case alphabets(y/n) : ";
+    cin>>choice;
     if(choice=='Y'||choice=='y')
     	s.push_back(upper);
 
-	cout<<"\nDo you want to include symbols(y/n) : ";
-	cin>>choice;
+    cout<<"\nDo you want to include symbols(y/n) : ";
+    cin>>choice;
     if(choice=='Y'||choice=='y')
     	s.push_back(symbol);
 
-	if(s.size()==0)
-		return cout<<"\nCan not generate password from empty domain",0;
+    if(s.size()==0)
+	return cout<<"\nCan not generate password from empty domain",0;
 		
     cout<<"\nEnter the length of the password : ";
     int length;
@@ -64,10 +64,6 @@ int main()
     	if(c=='y'||c=='Y')
     		break;
     }
-
-
-
-    
 
 	return 0;
 }
